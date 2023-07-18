569f412bcfd5fc18ae1b838a9b1fea4b4f79f716
ALi <Ha3er125@teachers.org>
2023-07-18 19:02:04 +0500

    Set fixes with routing

--- src/Components/All_Components.jsx
+++ src/Components/All_Components.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
 import Header from './Header'
 import Main_content from './Main_content'
-import {useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
-
 
 function All_Components() {
-  const navigate = useNavigate();
-
-  useEffect(()=>{
-    
-    if (!localStorage.getItem("useremail")){
-      navigate("/")
-    }
-  }
-  ,[])
   return (
     <div className='max-w-[1440px] w-full mx-auto px-3 py-1  '>
         <Header/>
